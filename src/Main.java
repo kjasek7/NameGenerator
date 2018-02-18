cbdacdfb6fa95f7b6f8008cec3305185f6a637bb
Kamil Jasek <kamil.r.jasek@gmail.com>
2018-02-18 20:43:34 +0100

    Zamiana na damksą wersje nazwiska

--- src/Main.java
+++ src/Main.java
@@ -4,33 +4,40 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        String [] firstName =  { "Anna", "Piotr", "Adam", "Monika", "Zbigniew", "Danuta",
+        final String [] firstName =  { "Anna", "Piotr", "Adam", "Monika", "Zbigniew", "Danuta",
                 "Jerzy", "Jadwiga", "Tadeusz", "Aleksandra", "Mateusz", "Halina",
                 "Dariusz", "Irena", "Mariusz"  };
-        String [] lastName = {"Nowak", "Kowalski", "Wiśniewski", "Wójcik", "Kowalczyk",
+        final String [] lastName = {"Nowak", "Kowalski", "Wiśniewski", "Wójcik", "Kowalczyk",
                 "Kamiński", "Lewandowski", "Zieliński", "Szymański", "Woźniak",
                 "Dąbrowski", "Kozłowski", "Jankowski", "Mazur", "Wojciechowski",
                 "Kwiatkowski", "Krawczyk", "Kaczmarek", "Piotrowski", "Grabowski" };
-
-        String [] gender = {"Female", "Male"};
 
         Random rand = new Random();
         Scanner scanner = new Scanner(System.in);
 
         String choice = scanner.next();
         String name;
+        String lastname =lastName[rand.nextInt(lastName.length)];
+
         name = firstName[rand.nextInt(firstName.length)];
-        if(choice.equals("Female"))
-            while(name.charAt(name.length()-1)!='a')
+        if(choice.equals("Female")) {
+            while (name.charAt(name.length() - 1) != 'a')
                 name = firstName[rand.nextInt(firstName.length)];
-        else
+
+            if( lastname.charAt(lastname.length()-1) == 'i')
+            {
+                lastname = lastname.substring(0,lastname.length()-1) + 'a';
+            }
+
+        }
+        else if(choice.equals("Male"))
             do{
                 name = firstName[rand.nextInt(firstName.length)];
             }
             while(name.charAt(name.length()-1)=='a');
 
         System.out.println("First Name: " + name );
-        System.out.println("Last Name: " + lastName[rand.nextInt(lastName.length)] );
+        System.out.println("Last Name: " + lastname );
 
     }
 }
